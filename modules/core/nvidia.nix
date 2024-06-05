6654e5baedfee2e9e95f4d0441b4f1a16631c17d
Namit Bhutani <namit.bhut@gmail.com>
2024-06-05 13:33:25 +0530

    fix: add back nvidia busID

--- modules/core/nvidia.nix
+++ modules/core/nvidia.nix
@@ -12,10 +12,12 @@
   			enable = true;
   			enableOffloadCmd = true;
   		};
+  		nvidiaBusId = "PCI:1:0:0";
   		intelBusId = "PCI:0:2:0";
   	};
   };
-    #   specialisation = {
+
+  #   specialisation = {
   #   nvidia-sync.configuration = {
   #     system.nixos.tags = [ "nvidia-sync" ];
   #     hardware.nvidia = {
