8ab173f698cb9f7023347cf1aa8805d2332363aa
Namit Bhutani <namit.bhut@gmail.com>
2025-07-03 07:46:22 +0530

    blacklist nvidia-drm nvidia-modeset to achieve D3Cold, set egl_vendor_library_filenames env to prevent hyprland from opening nvidia files

--- modules/core/nvidia.nix
+++ modules/core/nvidia.nix
@@ -1,6 +1,5 @@
 { pkgs, config, lib, ... }:
-
-{  
+{
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
@@ -8,27 +7,24 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
-  		offload = {
-  			enable = true;
-  			enableOffloadCmd = true;
-  		};
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
       sync.enable = false;
-  		nvidiaBusId = "PCI:1:0:0";
-  		intelBusId = "PCI:0:2:0";
-  	};
+      nvidiaBusId = "PCI:1:0:0";
+      intelBusId = "PCI:0:2:0";
+    };
   };
 
-        # boot.extraModprobeConfig = ''
-        #   blacklist nouveau
-        #   options nouveau modeset=0
-        # '';
-
-         boot.extraModprobeConfig = ''
+  boot.extraModprobeConfig = ''
     options nvidia "NVreg_DynamicPowerManagement=0x02"
-    '';
-        services.udev.extraRules = ''
+  '';
 
-  # Remove NVIDIA USB xHCI Host Controller devices, if present
+  boot.blacklistedKernelModules = [ "nvidia_drm" "nvidia_modeset" ];
+
+  services.udev.extraRules = ''
+    # Remove NVIDIA USB xHCI Host Controller devices, if present
   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{remove}="1"
   
   # Remove NVIDIA USB Type-C UCSI devices, if present
@@ -44,10 +40,5 @@
   # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
   ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
   ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
-
-
   '';
-
-        # boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
-
 }
