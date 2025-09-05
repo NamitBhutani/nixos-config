3e3ca4d0629c4b85f728c0767bae8525c7247a6c
Namit Bhutani <namit.bhut@gmail.com>
2025-09-05 17:23:42 +0530

    add hyprlock

--- modules/home/scripts/scripts/shutdown-script.sh
+++ modules/home/scripts/scripts/shutdown-script.sh
@@ -4,13 +4,9 @@ response=$(printf "Yes\nRestart\nNah" | fuzzel --dmenu)
 
 case "$response" in
     "Yes")
-        hyprctl dispatch exit &
-        sleep 1
         shutdown now
         ;;
     "Restart")
-        hyprctl dispatch exit &
-        sleep 1
         reboot
         ;;
     *)
