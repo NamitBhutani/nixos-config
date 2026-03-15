d47c278f2653075355c11926be40721c65985d10
Namit Bhutani <namit.bhut@gmail.com>
2026-03-15 13:06:25 +0530

    update

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -14,9 +14,14 @@
         "nix-command"
         "flakes"
       ];
-      # substituters = [ "https://wezterm.cachix.org" ];
-      # trusted-substituters = [ "https://wezterm.cachix.org" ];
-      # trusted-public-keys = [ "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0=" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://cuda-maintainers.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      ];
     };
     gc = {
       automatic = true;
